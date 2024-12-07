25e41d4fd295e33b5cbb5e889baaab7eec7ffcbb
Sanjay Nadhavajhala <sanjay.nadhavajhala@gmail.com>
2024-12-07 15:46:23 +0000

    update rubra v0.2

--- examples/extras/llama_pro/expand.sh
+++ examples/extras/llama_pro/expand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python scripts/llama_pro.py \
-    --model_name_or_path meta-llama/Meta-Llama-3.1-8B-Instruct \
-    --output_dir models/llama3.1-8b-pro \
-    --num_expand 4
+    --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
+    --output_dir models/llama3.2-3b-instruct-pro \
+    --num_expand 6

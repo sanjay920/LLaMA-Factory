3adde68fe48f49f19977eac3223e9a9b7a3c0383
Sanjay Nadhavajhala <sanjay@acorn.io>
2024-12-09 07:31:16 +0000

    update

--- examples/extras/llama_pro/expand.sh
+++ examples/extras/llama_pro/expand.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python scripts/llama_pro.py \
-    --model_name_or_path meta-llama/Llama-3.2-3B-Instruct \
-    --output_dir models/llama3.2-3b-instruct-pro \
-    --num_expand 6
+    --model_name_or_path meta-llama/Llama-3.2-1B-Instruct \
+    --output_dir models/llama3.2-1b-instruct-pro \
+    --num_expand 8
